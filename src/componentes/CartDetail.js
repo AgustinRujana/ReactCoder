47024d8daf59c8dfdcfb0ef4d8568f69bc960b6d
AgustinRujana <72451070+AgustinRujana@users.noreply.github.com>
2020-12-15 10:54:32 -0300

    Add files via upload

--- src/componentes/CartDetail.js
+++ src/componentes/CartDetail.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { Link } from 'react-router-dom';
 
 function CartDetail() {
   const [cartItems] = useContext(CartContext);
 
   return (
     <div>
-        {cartItems.length === 0 && <div>No hay productos.</div>}
+      {cartItems.length === 0 && <div>No hay productos. <Link to={'/'}>Volver Al Home</Link></div>}
       {cartItems.map((order, index) => {
         return (
           <div key={index}>
